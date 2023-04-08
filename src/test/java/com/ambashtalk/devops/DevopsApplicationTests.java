4c355e7aa2d6af36173834479dce6d3489543346
lohitya <ambashtalk@gmail.com>
2023-04-08 11:49:52 +0530

    chore: initialize spring project

---
+++ src/test/java/com/ambashtalk/devops/DevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ambashtalk.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
